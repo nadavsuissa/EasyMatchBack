5219e8dabdb592e74f3c4c7dd658510ce08e0cf4
nadav <naadavsuissa@gmail.com>
2022-11-24 14:21:08 +0200

    first

---
+++ src/main/java/com/easymatch/back/EasyMatchBackApplication.java
@@ -0,0 +1,13 @@
+package com.easymatch.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyMatchBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyMatchBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easymatch/back/EasyMatchBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easymatch.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyMatchBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
